f98fc8b57b06f8e27d7fbc800e6907737cc2da4f
Dmitry Kuchukov <kudim@inbox.ru>
2019-02-02 12:15:01 +0300

    -

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,55 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size=0;
 
     void clear() {
+        for (int i=0;i<size;i++){
+            storage[i]=null;
+        }
+        size=0;
     }
 
     void save(Resume r) {
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
+        }
+
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
+
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+                return;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
-    }
+
+            return new Resume[0];
+        }
+
+
 
     int size() {
-        return 0;
+        return size;
     }
 }
