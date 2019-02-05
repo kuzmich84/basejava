72eafe62ab214e351eea540547200d364c55252b
Dmitry Kuchukov <kudim84@yandex.ru>
2019-02-05 18:58:50 +0300

    push

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,29 +4,32 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000]; //58
+    Resume[] storage = new Resume[10000];
 
     int size = 0;
 
     void clear() {
-
         Arrays.fill(storage, null);
-
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
-
         size = 0;
     }
 
     void save(Resume r) {
         if (r.toString() != "" && r.toString() != null) {
+
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(r.toString())) {
+                    System.out.println("Resume already exists");
+                    return;
+                }
+            }
             storage[size] = r;
             size++;
+
+
+        } else {
+            System.out.println("Enter correct value");
         }
-
     }
-
 
 
     Resume get(String uuid) {
@@ -42,23 +45,29 @@ public class ArrayStorage {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 storage[i] = storage[size - 1];
-                storage[size-1] = null;
+                storage[size - 1] = null;
                 size--;
                 return;
             }
+        }
+        for (int i = 0; i < size; i++) {
+            if (!storage[i].uuid.equals(uuid)) {
+                System.out.println("Resume don't find");
+                break;
+            }
 
         }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage,size);
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-
         return size;
     }
 }
